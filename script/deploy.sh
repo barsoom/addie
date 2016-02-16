a3df19bd2167defd61d17c399c1603373941d2bf
Joakim Kolsjö and Tomas Skogberg <all+jocke+tomas@barsoom.se>
2016-02-16 11:15:38 +0100

    Add a reliable smoke testing setup

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 set -e
 
-git push git@heroku.com:${HEROKU_APP_NAME}.git $CIRCLE_SHA1:refs/heads/master
-ADDIE_URL=https://$HEROKU_APP_NAME.herokuapp.com script/smoke_test.sh
+revision=$(git rev-parse HEAD)
+
+function _main {
+  _deploy_to_heroku
+  _smoke_test
+}
+
+function _deploy_to_heroku {
+  git push git@heroku.com:${HEROKU_APP_NAME}.git $CIRCLE_SHA1:refs/heads/master
+  heroku config:set GIT_COMMIT=$revision -a $HEROKU_APP_NAME
+}
+
+function _smoke_test {
+  ruby script/ci/wait_for_new_revision_to_serve_requests.rb $HEROKU_APP_NAME $revision
+
+  echo
+  echo "Running smoke test:"
+
+  ADDIE_URL=https://${HEROKU_APP_NAME}.herokuapp.com script/smoke_test.sh
+}
+
+_main
