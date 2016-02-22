a1a1dc324d306086555e99291f667e0d119b5369
Joakim Kolsjö and Tomas Skogberg <all+jocke+tomas@barsoom.se>
2016-02-22 16:36:15 +0100

    Cleanup

--- app/app.rb
+++ app/app.rb
@@ -4,19 +4,19 @@ module Addie
       %{Address lookup service, see more at <a href="https://github.com/barsoom/addie">https://github.com/barsoom/addie</a>}
     end
 
-    get "/api/v1/lookup" do
-      LookupAddress.call(
-        street: params.fetch("street"),
-        country_code: params.fetch("country_code"),
-      ).to_json
-    end
-
     get "/revision" do
       ENV.fetch("GIT_COMMIT")
     end
 
     get "/boom" do
       raise "Sinatra has left the building"
+    end
+
+    get "/api/v1/lookup" do
+      LookupAddress.call(
+        street: params.fetch("street"),
+        country_code: params.fetch("country_code"),
+      ).to_json
     end
   end
 end
