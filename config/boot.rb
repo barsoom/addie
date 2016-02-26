c02bec81a567ac611b964dc34e3c0127b80794f4
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2016-02-26 12:01:46 +0100

    Disable honeybadger messages in test

--- config/boot.rb
+++ config/boot.rb
@@ -2,6 +2,9 @@
 RACK_ENV = ENV['RACK_ENV'] ||= 'development' unless defined?(RACK_ENV)
 PADRINO_ROOT = File.expand_path('../..', __FILE__) unless defined?(PADRINO_ROOT)
 
+# Disable honeybadger messages in test
+ENV["HONEYBADGER_LOGGING_LEVEL"] = "error" if RACK_ENV == "test"
+
 # Load our dependencies
 require "bundler/setup"
 Bundler.require(:default, RACK_ENV)
