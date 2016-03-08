1ce910d75f1bf9b7c888bea7f4f04c22f0fb255b
Tomas Skogberg <tomas.skogberg@gmail.com>
2016-03-08 10:44:23 +0100

    Update CI scripts
    To be compatible with an environment created by "dev create"

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
 set -e
 
+# Add config for heroku commands like "heroku config:set" and git based deploy over https
+echo -e "machine api.heroku.com\n  login $HEROKU_API_USER\n  password $HEROKU_API_TOKEN\nmachine code.heroku.com\n  login $HEROKU_API_USER\n  password $HEROKU_API_TOKEN\nmachine git.heroku.com\n  login $HEROKU_API_USER\n  password $HEROKU_API_TOKEN" > ~/.netrc
+chmod 0600 ~/.netrc
+
+cd ~/$CIRCLE_PROJECT_REPONAME
+
+# Deploy
 revision=$(git rev-parse HEAD)
+app_name=$1
 
 function _main {
   _deploy_to_heroku
@@ -9,17 +18,18 @@ function _main {
 }
 
 function _deploy_to_heroku {
-  git push git@heroku.com:${HEROKU_APP_NAME}.git $CIRCLE_SHA1:refs/heads/master
-  heroku config:set GIT_COMMIT=$revision -a $HEROKU_APP_NAME
+  heroku git:remote --app $app_name
+  git push heroku master
+  heroku config:set GIT_COMMIT=$revision -a $app_name
 }
 
 function _smoke_test {
-  ruby script/ci/wait_for_new_revision_to_serve_requests.rb $HEROKU_APP_NAME $revision
+  ruby script/support/wait_for_new_revision_to_serve_requests.rb $app_name $revision
 
   echo
   echo "Running smoke test."
 
-  ADDIE_URL=https://${HEROKU_APP_NAME}.herokuapp.com script/smoke_test.sh
+  APP_URL=https://$app_name.herokuapp.com script/smoke_test.sh
 }
 
 _main
--- script/smoke_test.sh
+++ script/smoke_test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -s "$ADDIE_URL/api/v1/look_up?country_code=SE&street=Kungs" | grep Norrby 1> /dev/null || exit 1
+curl -s "$APP_URL/api/v1/look_up?country_code=SE&street=Kungs" | grep Norrby 1> /dev/null || exit 1
